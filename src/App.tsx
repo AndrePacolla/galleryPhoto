44671b058c63a874f556856049b51320867af04c
AndrePacolla <andreluizzpacolla@outlook.com>
2025-01-23 20:06:46 -0300

    openModal

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,40 @@
-import './App.css'
+import { useState } from 'react'
+import { PhotoItem } from './components/PhotoItem'
+import { photoList } from './data/photoList'
 
 function App() {
+
+  const [showModal, setShowModal] = useState(false);
+  const [imageOfModal, setImageOfModal] = useState("");
+
+
+  const openModal = (id: Number) => {
+    const photo = photoList.find(item => item.id === id);
+    if(photo){
+      setImageOfModal(photo.url);
+      setShowModal(true);
+    }
+  }
 
 
   return (
     <>
-     <h1 className='text-red-500'>Fotos Intergalaticas</h1>
+      
+        <h1 className='text-white text-center my-14 text-3xl font-bold'>Fotos Intergalacticas</h1>
+        <section className='container max-w-5xl mx-auto grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8 '>
+          {photoList.map(item => (
+            <PhotoItem
+
+              key={item.id}
+              photo={item}
+              onClick={() => openModal(item.id)}
+            />
+          ))}
+        </section>
+
+        {showModal && 
+        <Modal  image = {imageOfModal} closeModal={closeModal}/>
+        }
     </>
   )
 }
